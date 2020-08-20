2c1453c3069726543c291b7793feb46d87c54494
phronesys <daniel.dazarola@gmail.com>
2020-08-20 13:12:06 -0400

    uwu 20/08

--- Data-Structures/queue.java
+++ Data-Structures/queue.java
@@ -6,3 +6,53 @@
         .peek (look at the last)
         
 */
+
+class linkedQueue{
+    private class Node {
+        int data;
+        Node next;
+    }
+    
+    Node front;
+    Node back;
+    linkedQueue() { this.front = this.back = null; }
+
+    public boolean isEmpty() { 
+        if (front == null && back == null) 
+            return true;
+        else 
+            return false;
+    }
+    public int peek()
+    {
+        if (!isEmpty()) return back.data;
+        System.out.println("\nQueue empty");
+        return -1;
+    }
+    public void dequeue()
+    {
+        if (isEmpty()) {
+            System.out.println("\nQueue empty");
+            return;
+        }
+         
+
+    }
+    public void enqueue()
+    {
+        
+    }
+    public void printQueue()
+    {
+
+    }
+}
+
+class queue{
+
+    public static void main(String[] args)
+        {
+        
+        }
+
+}
