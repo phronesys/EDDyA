52bafced670acf6474a576b41d734f08ccc4becd
phronesys <daniel.dazarola@gmail.com>
2020-08-21 00:49:53 -0400

    uwu 21/08

--- Data-Structures/queue.java
+++ Data-Structures/queue.java
@@ -4,30 +4,81 @@
         .dequeue (remove peek)
         .enqueue (push)
         .peek (look at the last)
-        
+        .printQueue
 */
+import java.util.*;
+
+public class queue{
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        linkedQueue test = new linkedQueue();
+        boolean on = true;
+        
+        do{
+            System.out.println("\n1.- Enqueue");
+            System.out.println("2.- Dequeue");
+            System.out.println("3.- Peek");
+            System.out.println("4.- Print Queue");
+            switch (scan.nextInt()) {
+                case 1:
+                    test.enqueue(scan.nextInt());
+                    break;
+                case 2:
+                    test.dequeue();
+                    break;
+                case 3:
+                    System.out.println(test.peek());    
+                    break;
+                case 4:
+                    test.printQueue();
+                    break;
+                default:
+                    break;
+            }
+        }while(on);
+        scan.close();
+    }
+
+}
 
 class linkedQueue{
     private class Node {
         int data;
         Node next;
     }
-    
-    Node front;
-    Node back;
-    linkedQueue() { this.front = this.back = null; }
+    //      last      first
+    // null<- 3 <- 2 <- 1 
+    Node first;
+    Node last;
+    linkedQueue() { this.first = this.last = null; }
 
     public boolean isEmpty() { 
-        if (front == null && back == null) 
+        if (first == null ) 
             return true;
         else 
             return false;
     }
     public int peek()
     {
-        if (!isEmpty()) return back.data;
+        if (!isEmpty()) return first.data;
         System.out.println("\nQueue empty");
         return -1;
+    }
+    public void enqueue(int x)
+    {
+        Node temp = new Node();
+        temp.data = x;
+        temp.next = null;
+
+        if (isEmpty()){
+            last = first = temp;
+        }else {
+            last.next = temp;
+            last = temp;
+        }
     }
     public void dequeue()
     {
@@ -35,24 +86,21 @@ class linkedQueue{
             System.out.println("\nQueue empty");
             return;
         }
-         
-
-    }
-    public void enqueue()
-    {
-        
+        Node temp = new Node();
+        temp = first;
+        first = first.next;
+        temp = null;
     }
     public void printQueue()
     {
-
+        Node temp = new Node();
+        temp = first;
+        while(temp != null)
+        {
+            System.out.printf("%d ->", temp.data);
+            temp = temp.next;
+        }
     }
 }
 
-class queue{
 
-    public static void main(String[] args)
-        {
-        
-        }
-
-}
