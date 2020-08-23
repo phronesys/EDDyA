2a137d1b07d927934ca7d0e6e61142e482e99879
phronesys <daniel.dazarola@gmail.com>
2020-08-23 19:34:11 -0400

    uwu 23/08

--- Lab/test.java
+++ Lab/test.java
@@ -1,6 +1,14 @@
-class CSVread {
+import java.util.ArrayList;
+import java.io.*;
 
+
+class test {
     public static void main(String[] args) {
         
+    }
+    public static ArrayList readCSV() {
+        ArrayList test = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(""));
+
     }
 }
