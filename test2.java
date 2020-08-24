05fa6501854bbf9952ded377ba309e665b0d0144
phronesys <daniel.dazarola@gmail.com>
2020-08-23 21:13:06 -0400

    ewe 23/08

---
+++ test2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+// ArrayList x ArrayList
+// adding csv to ALxAL
+
+import java.utils.*;
+import java.io.*;
+
+public class test2 {
+
+    public static void main(String[] args) {
+        
+
+    }
+
+
+    /*
+    ????????????  
+    public void readFile() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("teambans.csv"));
+        lines = new ArrayList<ArrayList>();
+        String newLine;
+        while ((newLine = br.readLine()) != null) {
+
+            System.out.println(newLine);
+            lines.add(newLine);
+        }
+        br.close();
+    } */
+}
