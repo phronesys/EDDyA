b70c6ffb7907b0d7cf3d236f21eb867f475836fc
Daniel <daniel.dazarola@mail.com>
2020-09-05 01:28:24 -0400

    8gb de ram no son suficiente t-t fuck java

--- Lab/Project/main.java
+++ Lab/Project/main.java
@@ -7,15 +7,45 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> parsed = new ArrayList<ArrayList<String>>();
-        String csvString = "./matches.csv";
-        parser(csvString, parsed);
-        // write in a .csv
-        writer(parsed);
-        print(parsed);
+        String matches = "./matches.csv",
+               champs = "./champs.csv",
+               participants = "./participants.csv",
+               stats1 = "./stats1.csv",
+               stats2 = "./stats2.csv",
+               teambans = "./teambans.csv",
+               teamstats = "./teamstats.csv";
+        String match = "matchesOut.csv";
+        String champ = "champsOut.csv";
+        String parti = "participantsOut.csv";
+        String stat1 = "stats1Out.csv";
+        String stat2 = "stats2Out.csv";
+        String tmban = "teambansOut.csv";
+        String tstat = "teamstatsOut.csv";
+        
+        /* ArrayList<ArrayList<String>> parsed1 = new ArrayList<ArrayList<String>>();
+        parser(matches, parsed1);
+        writer(match, parsed1);
+        ArrayList<ArrayList<String>> parsed2 = new ArrayList<ArrayList<String>>();
+        parser(champs, parsed2);
+        writer(champ, parsed2);
+        ArrayList<ArrayList<String>> parsed3 = new ArrayList<ArrayList<String>>();
+        parser(participants, parsed3);
+        writer(parti, parsed3); */
+        /* ArrayList<ArrayList<String>> parsed4 = new ArrayList<ArrayList<String>>();
+        parser(stats1, parsed4);
+        writer(stat1, parsed4);  */
+        ArrayList<ArrayList<String>> parsed5 = new ArrayList<ArrayList<String>>();
+        parser(stats2, parsed5);
+        //writer(stat2, parsed5);  
+        /* ArrayList<ArrayList<String>> parsed6 = new ArrayList<ArrayList<String>>();
+        parser(teambans, parsed6);
+        writer(tmban, parsed6);
+        ArrayList<ArrayList<String>> parsed7 = new ArrayList<ArrayList<String>>();
+        parser(teamstats, parsed7);
+        writer(tstat, parsed7); */
+
     }
     static void print(ArrayList<ArrayList<String>> parsed){
-        // printing what is inside 2d arraylist
         for(int i = 0; i < parsed.size(); i++){
             for (int j = 0; j < parsed.get(i).size(); j++){
                 System.out.print(parsed.get(i).get(j));
@@ -23,11 +53,11 @@ public class main {
             System.out.println();
         }
     }
-    static void writer(ArrayList<ArrayList<String>> parsed){
+    static void writer(String name, ArrayList<ArrayList<String>> parsed){
         int i = 0;
         BufferedWriter writer;
         try {
-            writer = new BufferedWriter(new FileWriter("output.csv", false));
+            writer = new BufferedWriter(new FileWriter(name, false));
             // cada arraylist de la matriz
             for(ArrayList items : parsed) 
             {
@@ -45,23 +75,23 @@ public class main {
             err.printStackTrace();
         }
     }
-
     static void parser(String fileLocation, ArrayList<ArrayList<String>> parsed){
         BufferedReader reader;
         try {
             // inicializamos reader con el string correspondiente al archivo
             reader = new BufferedReader(new FileReader(fileLocation));
-            String line = reader.readLine();
-            while(line != null) {
+            String line;
+            while(( line = reader.readLine()) != null) {
                 // items separados por comas
                 String[] splited;
+                System.out.println(line);
                 splited = line.split(",", -1);
                 ArrayList<String> lines = new ArrayList<String>();
                 for(int i = 0; i < splited.length; i++){
                     lines.add(splited[i]);
                 }
                 parsed.add(lines);
-                line = reader.readLine();
+                //line = reader.readLine();
             }
             reader.close();
         } catch (IOException error) {
