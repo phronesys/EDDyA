1b3b4a522acac3d30c1aed6f771c42861dc750d1
Daniel <daniel.dazarola@mail.com>
2020-09-06 20:42:30 -0300

    8gb de ram no son suficiente t-t fuck java

--- Lab/Project/main.java
+++ Lab/Project/main.java
@@ -4,46 +4,53 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class main {
     public static void main(String[] args) {
         String matches = "./matches.csv",
-               champs = "./champs.csv",
-               participants = "./participants.csv",
-               stats1 = "./stats1.csv",
-               stats2 = "./stats2.csv",
-               teambans = "./teambans.csv",
-               teamstats = "./teamstats.csv";
-        String match = "matchesOut.csv";
-        String champ = "champsOut.csv";
-        String parti = "participantsOut.csv";
-        String stat1 = "stats1Out.csv";
-        String stat2 = "stats2Out.csv";
-        String tmban = "teambansOut.csv";
-        String tstat = "teamstatsOut.csv";
-        
-        /* ArrayList<ArrayList<String>> parsed1 = new ArrayList<ArrayList<String>>();
+                champs = "./champs.csv",
+                participants = "./participants.csv",
+                stats1 = "./stats1.csv",
+                stats2 = "./stats2.csv",
+                teambans = "./teambans.csv",
+                teamstats = "./teamstats.csv",
+                match = "outMatches.csv",
+                champ = "outChamps.csv",
+                parti = "outParticipants.csv",
+                stat1 = "outStats1.csv",
+                stat2 = "outStats2.csv",
+                tmban = "outTeambans.csv",
+                tstat = "outTeamstats.csv";
+        System.out.println("parsing every file");
+        ArrayList<ArrayList<String>> parsed1 = new ArrayList<ArrayList<String>>();
         parser(matches, parsed1);
         writer(match, parsed1);
+        System.out.println("parsed 1");
         ArrayList<ArrayList<String>> parsed2 = new ArrayList<ArrayList<String>>();
         parser(champs, parsed2);
         writer(champ, parsed2);
+        System.out.println("parsed 2");
         ArrayList<ArrayList<String>> parsed3 = new ArrayList<ArrayList<String>>();
         parser(participants, parsed3);
-        writer(parti, parsed3); */
-        /* ArrayList<ArrayList<String>> parsed4 = new ArrayList<ArrayList<String>>();
+        writer(parti, parsed3); 
+        System.out.println("parsed 3");
+        ArrayList<ArrayList<String>> parsed4 = new ArrayList<ArrayList<String>>();
         parser(stats1, parsed4);
-        writer(stat1, parsed4);  */
+        writer(stat1, parsed4);
+        System.out.println("parsed 4");
         ArrayList<ArrayList<String>> parsed5 = new ArrayList<ArrayList<String>>();
         parser(stats2, parsed5);
-        //writer(stat2, parsed5);  
-        /* ArrayList<ArrayList<String>> parsed6 = new ArrayList<ArrayList<String>>();
+        writer(stat2, parsed5);  
+        System.out.println("parsed 5");
+        ArrayList<ArrayList<String>> parsed6 = new ArrayList<ArrayList<String>>();
         parser(teambans, parsed6);
         writer(tmban, parsed6);
+        System.out.println("parsed 6");
         ArrayList<ArrayList<String>> parsed7 = new ArrayList<ArrayList<String>>();
         parser(teamstats, parsed7);
-        writer(tstat, parsed7); */
-
+        writer(tstat, parsed7); 
+        System.out.println("parsed 7");
     }
     static void print(ArrayList<ArrayList<String>> parsed){
         for(int i = 0; i < parsed.size(); i++){
@@ -83,15 +90,12 @@ public class main {
             String line;
             while(( line = reader.readLine()) != null) {
                 // items separados por comas
-                String[] splited;
-                System.out.println(line);
-                splited = line.split(",", -1);
+                StringTokenizer tokenizer = new StringTokenizer(line, ",");
                 ArrayList<String> lines = new ArrayList<String>();
-                for(int i = 0; i < splited.length; i++){
-                    lines.add(splited[i]);
+                while(tokenizer.hasMoreTokens()) {
+                    lines.add(tokenizer.nextToken());
                 }
                 parsed.add(lines);
-                //line = reader.readLine();
             }
             reader.close();
         } catch (IOException error) {
