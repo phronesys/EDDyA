c58b06cc5940bb6544178f106603cdd2481e0955
phronesys <daniel.dazarola@gmail.com>
2020-09-08 21:00:04 -0300

    fuck java

--- Lab/Project/App.java
+++ Lab/Project/App.java
@@ -93,20 +93,22 @@ public class App {
                     System.out.println("uwu");
                     // esto se ejecuta cuando cambia al siguiente matchid
                     // aca debería llamar una función que ordene lo que no está ordenado
-                    fixThis(pares);
+                    fixThis(pares, ordenado);
                     // pasa lo ordenado a ordenado
-                    
+                    ordenado.printQueue();
                 }
                 ordenado = new queue();
                 System.out.println("match: " + imatchid);
                 currentmatchid = imatchid;
                 // add queue currentmatchid 
-                ordenado.enqueue(currentmatchid);
+                // currentmatchid -> file
+                // todo el queue -> file
+                ordenado.enqueue(imatchid);
                 System.out.println(pair.get(0) + " " + pair.get(1));
-                ordenado.enqueue(pair.get(1));
+                // ordenado.enqueue(pair.get(1));
                 // el primer item sería el 1 , por lo que se debería agregar
                 // de una al queue
-            
+                
                 
             } else {
                 System.out.println(pair.get(0) + " " + pair.get(1));
@@ -120,28 +122,31 @@ public class App {
         }  
         
     }
-    static void fixThis(ArrayList<List<Integer>> desorden){
-        //int[] array = new int[6];
+    static void fixThis(ArrayList<List<Integer>> desorden, queue ordenado){
         
         ArrayList<Integer> orden = new ArrayList<Integer>();
-        //System.out.println(desorden.size()); = 5
-
-        // Insertion sort
-        // banturn
-        // desorden.get(i).get(1) champid
-        // 3 5 2 4 6 buscamos el menor
-        // encuentra 2
-        // orden.add(desorden.get(i).get(1))
-        // 3 4 
-        int x = 2;
-        int n = desorden.size();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(desorden.get(x).get(0)  == x)
+        int index = 0;
+        int buscado = 2;
+        while(!desorden.isEmpty()){
+            if(buscado == desorden.get(index).get(0))
+            {
+                orden.add(desorden.get(index).get(1));
+                desorden.remove(index);
+                index = 0;
+                buscado++;
+            } else {
+                index++;
+            }
+            if(index > desorden.size()-1){
+                buscado++;
+                index = 0;
             }
         }
-        
-        
+
+        for(int i : orden){
+            ordenado.enqueue(i);
+        }
+        orden.clear();
         desorden.clear();
     }
     static void writer(String name, ArrayList<ArrayList<String>> parsed){
@@ -184,24 +189,21 @@ public class App {
             error.printStackTrace();
         }
     }
-    static void parserUsingSplit(String fileLocation, ArrayList<ArrayList<String>> parsed){
-        BufferedReader reader;
+    static void writerQueue(String out, queue linea, int matchid){
+        BufferedWriter writer;
         try {
-            // inicializamos reader con el string correspondiente al archivo
-            reader = new BufferedReader(new FileReader(fileLocation));
-            String line;
-            while(( line = reader.readLine()) != null) {
-                // items separados por comas
-                String splited[] = line.split(",", -1);
-                ArrayList<String> lines = new ArrayList<String>();
-                for(int i = 0; i < splited.length; i++){
-                    lines.add(splited[i]);
-                }
-                parsed.add(lines);
+            writer = new BufferedWriter(new FileWriter(out, false));
+            writer.write(matchid);
+            writer.write(":");
+            while(!linea.isEmpty()){
+                writer.write((Integer)linea.front());
+                linea.dequeue();
+                if(!linea.isEmpty()) 
+                    writer.write(",");
             }
-            reader.close();
-        } catch (IOException error) {
-            error.printStackTrace();
+            writer.close();
+        } catch (IOException err) {
+            err.printStackTrace();
         }
     }
     static String noQuotes(String item){
