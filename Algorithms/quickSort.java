52486415cfe2e3db9b8ef4c619a4e5a86733b3eb
phronesys <daniel.dazarola@gmail.com>
2020-10-06 01:08:15 -0300

    paren el semestre

---
+++ Algorithms/quickSort.java
@@ -0,0 +1,10 @@
+public class quickSort {
+    public static void main(String[] args) {
+        int[] a = {1, 10, 9, 7, 8, 5, 6, 4, 3, 2};
+        drawer.printArray(a);
+    
+    }
+    public static void QuickSort(){
+        
+    }
+}
--- Tareas/Tarea4/ecuacion.java
+++ Tareas/Tarea4/ecuacion.java
@@ -1,39 +1,41 @@
 import java.util.Scanner;
 import java.lang.Math;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 
 public class ecuacion {
     public static void main(String[] args) {
         int N;
+        int a, b, c, d, e, f;
         Scanner scan = new Scanner(System.in);
-
+        DecimalFormat df = new DecimalFormat("#.####");
+        df.setRoundingMode(RoundingMode.CEILING);
         N = scan.nextInt();
-        //int[] cases = new int[N];
-        ArrayList cases = new ArrayList();
+        
         for(int i = 0; i < N; i++){
+            boolean flag = false;
             a = scan.nextInt(); 
             b = scan.nextInt(); 
             c = scan.nextInt(); 
             d = scan.nextInt(); 
             e = scan.nextInt(); 
             f = scan.nextInt(); 
-            cases.add(solve(a,b,c,d,e,f));
+            for(double x = 0.0000; x <= 1.0; x += 0.0001){
+                double ecu = (((float)a*Math.pow(Math.E, -(x))) + ((float)b*Math.sin(x)) + ((float)c*Math.cos(x)) + ((float)d*Math.tan(x)) + ((float)e*Math.pow(x, 2)) + f);
+                
+                if(ecu < 0.0001){
+                    double num = Math.round(x * 10000d) /10000d;
+                    System.out.println(num);
+                    flag = true;
+                    break;
+                }
+            }  
+            if(!flag){
+                System.out.println("No existe");
+            }
         }
+        
+        scan.close();
     }
-    public static int solve(int a, int b, int c, int d, int e, int f)
-    {
-        int x;
-
-
-
-
-        return 0;
-    }
-    // funciones a usar
-    public static float aFun(int x){ return a*Math.pow(Math.E, -(x));}
-    public static float bFun(int x){ return b*Math.sin(x);}
-    public static float cFun(int x){ return c*Math.cos(x);}
-    public static float dFun(int x){ return d*Math.tan(x);}
-    public static float eFun(int x){ return e*Math.pow(x,2);} 
-    public static float fFun(int x){ return f;}
-    public static int a, b, c, d, e, f;
 }
