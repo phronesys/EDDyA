f94c56f54e9a3f97e728ff5caedbf1f621faad37
Daniel Dazarola <daniel.dazarola@gmail.com>
2020-10-20 04:12:41 -0300

    heapSort

---
+++ Algorithms/heapSort.java
@@ -0,0 +1,21 @@
+/**
+ * heapSort
+ */
+public class heapSort {
+
+    public static void main(String[] args) {
+        
+    }
+    public void sort(int arr[])
+    {
+        int n = arr.length;
+
+        for(int i = n / 2 - 1; i >= 0; i --)
+            heapify(arr, n, i);
+    }
+    // this function converts a part of the heap in a max heap (top-down)
+    public void heapify(int arr[], int n, int i)
+    {
+
+    }
+}
