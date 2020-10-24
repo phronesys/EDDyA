a5ffc52e895c36b7f9a30d6d4396c431ae8423f5
phronesys <daniel.dazarola@gmail.com>
2020-10-24 12:15:19 +0000

    volcado lab1

--- Lab/Lab2/Project/volcado.java
+++ Lab/Lab2/Project/volcado.java
@@ -12,13 +12,44 @@ import java.util.*;
 public class volcado {
     public static void main(String[] args) {
         BufferedReader reader;
+        BufferedWriter writer;
         //String[] filenames = {"./stats1.csv","./stats2.csv","./participants.csv","./champs.csv"};
         String[] filenames = {"./stats1.csv","./stats2.csv"}; // en ambos quiero 0 y 21
-        ArrayList<ArrayList<String>> dataset = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> dataset = new ArrayList<ArrayList<String>>(); // matriz
+        String outfilename = "stats_out.csv";
         try
-        {
+        {   
+            // lee los 2 stats por separado
             for(String fn : filenames){
-                
+                reader = new BufferedReader(new FileReader(fn));
+                writer = new BufferedWriter(new FileWriter(outfilename, false));
+                String line = reader.readLine();
+                boolean uwu = false;
+                while(line != null)
+                {
+                    ArrayList<String> parsing1 = new ArrayList<String>();
+                    String[] row1 = line.split(",", -1); 
+                    // de cada linea añade lo que hay entre comas a un array de string
+                    if(uwu){ // se omite la primera linea 
+                        // se borran las comillas de los valores que necesito
+                        row1[0] = row1[0].replace("\"","");     // id           
+                        row1[21] = row1[21].replace("\"","");   // totdmgdealt
+                        parsing1.add(row1[0]);
+                        parsing1.add(row1[21]);
+                        dataset.add(parsing1); // añadido el par que se necesita en este caso
+                    } 
+                    uwu = true;
+                    line = reader.readLine();   // sig linea
+                }
+                for(ArrayList x : dataset)
+                {
+                    writer.write(x.get(0).toString());
+                    writer.write(",");
+                    writer.write(x.get(1).toString());
+                    writer.write("\n");
+                }
+                reader.close();
+                writer.close();
             }
         }catch(IOException e){
             e.printStackTrace();
