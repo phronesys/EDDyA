a0fa181083524afbb518df7566e1ac47f26073c0
phronesys <daniel.dazarola@gmail.com>
2020-10-24 11:49:59 +0000

    avances profe

---
+++ Lab/Lab2/Project/volcado.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+/*
+ * Esto no es una copia :v
+ * Usé como referencia el codigo otorgado por el profesor (lab a lab)
+ * para así construir la misma idea desde la base. :p
+ * 
+ */
+
+public class volcado {
+    public static void main(String[] args) {
+        BufferedReader reader;
+        //String[] filenames = {"./stats1.csv","./stats2.csv","./participants.csv","./champs.csv"};
+        String[] filenames = {"./stats1.csv","./stats2.csv"}; // en ambos quiero 0 y 21
+        ArrayList<ArrayList<String>> dataset = new ArrayList<ArrayList<String>>();
+        try
+        {
+            for(String fn : filenames){
+                
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+}
