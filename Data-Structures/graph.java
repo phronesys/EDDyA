eec4f36b70823ec4dd03e9915fef26f1b864360a
phronesys <daniel.dazarola@gmail.com>
2020-11-18 20:34:18 -0300

    graph java

---
+++ Data-Structures/graph.java
@@ -0,0 +1,39 @@
+/* 
+    ref:
+        https://www.softwaretestinghelp.com/java-graph-tutorial/
+        https://www.baeldung.com/java-graphs
+*/
+import java.util.*;
+
+// class to store aristas (edges) of the eighted graph
+class Arista {
+    int src, dest, weight;
+    Arista(int desde, int hacia, int peso){
+        this.src = desde;
+        this.dest = hacia;
+        this.weight = peso;
+    }
+}
+class Graph {
+    static class NodeGraph {
+        int value, weight; 
+        NodeGraph(int value, int weight) {
+            this.value = value;
+            this.weight = weight;
+        }
+    }
+    List<List<NodeGraph>> adj_list = new ArrayList<>();
+
+    public Graph(List<Arista> aristas){
+        for (int i = 0; i < aristas.size(); i++)
+            adj_list.add()
+    }
+}
+
+ // main
+public class graph {
+    public static void main(String[] args) {
+        
+    }
+}
+
